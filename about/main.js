529db84fa54159f79e5de69c1188c63d987c212a
Toluwalope <88102300+Victorious-Toluwalope@users.noreply.github.com>
2024-07-31 18:07:56 +0100

    Why Aletheia section and images

---
+++ about/main.js
@@ -0,0 +1,31 @@
+// Animation upon scroll for Why Aletheia Components
+
+document.addEventListener('DOMContentLoaded', function () {
+  
+  // Function which adds the 'animated' class to any '.animatable' in view
+  var doAnimations = function() {
+    
+    // Calc current offset and get all animatables
+    var offset = window.scrollY + window.innerHeight;
+    var animatables = document.querySelectorAll('.animatable');
+    
+    // Unbind scroll handler if we have no animatables
+    if (animatables.length === 0) {
+      window.removeEventListener('scroll', doAnimations);
+    }
+    
+    // Check all animatables and animate them if necessary
+    animatables.forEach(function(animatable) {
+      if ((animatable.getBoundingClientRect().top + window.scrollY + animatable.offsetHeight - 20) < offset) {
+        animatable.classList.remove('animatable');
+        animatable.classList.add('animated');
+      }
+    });
+
+  };
+  
+  // Hook doAnimations on scroll, and trigger a scroll
+  window.addEventListener('scroll', doAnimations);
+  window.dispatchEvent(new Event('scroll'));
+
+});
